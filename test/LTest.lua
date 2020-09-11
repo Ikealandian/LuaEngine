ee488f68812a10f525222be203b3cd035b4edfd3
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-11 20:56:11 +0200

    add Wait function

--- test/LTest.lua
+++ test/LTest.lua
@@ -1,13 +1,21 @@
 local idx = 0
 local running = true
 
+function wait(seconds)
+    wait_time = os.time() + seconds
+    while(os.time() < wait_time) do
+    end
+end
+
 while running do
 
     print("idx: ", idx)
     idx = idx + 1
 
-    if idx > 300 then
+    if idx > 10 then
         running = false
     end
+
+    wait(1);
 
 end
