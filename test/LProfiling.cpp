4928bf3a40f1a41f29473a82fff87bb9210b868e
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-11 23:27:06 +0200

    Profile Allocations

--- test/LProfiling.cpp
+++ test/LProfiling.cpp
@@ -1,10 +1,12 @@
 #include "LProfiling.hpp"
+#include "LAllocTest.hpp"
 
 // 
 // Profiler
 //
 
-Profiler::Profiler()
+Profiler::Profiler(LAlloc* _Alloc)
+    : lAlloc(_Alloc)
 {
 }
 
@@ -17,7 +19,7 @@ void Profiler::StopProfiling(const char* _ProfileName) {}
 
 ScopeProfiler Profiler::ProfileThisScope(const char* _ProfileName)
 {
-    return ScopeProfiler(_ProfileName, this);
+    return ScopeProfiler(_ProfileName, this, lAlloc);
 }
 
 Profile Profiler::GetProfileData(const char* _ProfileName)
@@ -29,7 +31,7 @@ Profile Profiler::GetProfileData(const char* _ProfileName)
 void Profiler::PrintProfileData(const char* _ProfileName)
 {
     Profile P = GetProfileData(_ProfileName);
-    printf("Profile: %s\n\tTime:\t%fms\n", _ProfileName, P.Ms);
+    printf("Profile: %s\n\tTime:\t\t%fms\n\tAllocated:\t%dB\n", _ProfileName, P.Ms, P.Allocated);
 }
 
 void Profiler::SetProfile(const char* _ProfileName, Profile _Profile)
@@ -41,10 +43,14 @@ void Profiler::SetProfile(const char* _ProfileName, Profile _Profile)
 // Scope Profiler
 //
 
-ScopeProfiler::ScopeProfiler(const char* _Profile, Profiler* _Parent)
+ScopeProfiler::ScopeProfiler(const char* _Profile, Profiler* _Parent, struct LAlloc* _PAlloc)
 {
     pProfiler = _Parent;
+    pAlloc = _PAlloc;
+    
     sProfile.Name = _Profile;
+    aBefore = pAlloc->Allocated;
+    
     pStart = std::chrono::steady_clock::now();
 }
 
@@ -52,6 +58,9 @@ ScopeProfiler::~ScopeProfiler()
 {
     pEnd = std::chrono::steady_clock::now();
     std::chrono::duration<double, std::milli> ScopeMS = pEnd - pStart;
+
     sProfile.Ms = ScopeMS.count();
+    sProfile.Allocated = pAlloc->Allocated - aBefore;
+
     pProfiler->SetProfile(sProfile.Name, sProfile);
 }
