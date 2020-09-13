feb518136106f9f4335a77b1280e88671d6624e9
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-13 06:08:31 +0200

    lua_State Smart Pointer

--- test/Wrapping/LWrapping.cpp
+++ test/Wrapping/LWrapping.cpp
@@ -4,9 +4,53 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <memory>
+
+// Include Lua.hpp
+#include <lua.hpp>
+
+/** - Lua Types -
+ * number
+ * string
+ * table
+ * boolean
+ * userdata
+ * light userdata
+ * function
+ * thread
+ * nil
+ **/
+
+/**
+ * lua_State Smart Pointer handler
+**/
+struct LuaStateHandler
+{
+    lua_State* lState;
+
+    LuaStateHandler()
+    {
+        // TODO:
+        // USE lua_newstate and create an Allocator
+        lState = luaL_newstate();
+    }
+
+    ~LuaStateHandler()
+    {
+        lua_close(lState);
+    }
+};
+
+/**
+ * lua_State Smart Pointer
+ **/
+using LStatePtr = std::unique_ptr<lua_State, LuaStateHandler>;
 
 int main()
 {
+    // Create a new Lua state
+    // NOTE: State automatically closes at end of scope
+    LStatePtr L;
 
     
 
