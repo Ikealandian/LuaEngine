d370fbd57a5f9e7883e9a0d5e9a87c4f0abca957
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-13 07:13:00 +0200

    Add LuaCall and run test script

--- test/Wrapping/LWrapping.cpp
+++ test/Wrapping/LWrapping.cpp
@@ -22,8 +22,13 @@
  **/
 
 /**
+ * Useful macros
+ **/
+#define LUA_TOP -1
+
+/**
  * lua_State Smart Pointer
-**/
+ **/
 struct LStatePtr
 {
     // lua_State*
@@ -57,13 +62,36 @@ struct LStatePtr
 
 };
 
+/**
+ * Verify the return value of Lua function
+ **/
+bool LuaCall(lua_State* _State, int _CallResult)
+{
+    if (_CallResult == LUA_OK)
+        return true;
+
+    printf("Lua [Error]: %s\n", lua_tostring(_State, LUA_TOP));
+
+    return false;
+}
+
+/**
+ * Useful Lua function macros
+ **/
+#define L_LoadFile(L, F) LuaCall(L, luaL_loadfile(L, F))
+#define L_LRunFile(L, F) LuaCall(L, luaL_dofile(L, F))
+
 int main()
 {
     // Create a new Lua state
     // NOTE: State automatically closes at end of scope
     LStatePtr L;
-
     
+    // Open Lua libraries
+    luaL_openlibs(L);
+
+    // Run the test script
+    L_LRunFile(L, "test/Wrapping/test.lua");
 
     return 0;
 }
