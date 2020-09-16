7b5c1d38175aa048c5e2ea703f3b23d6dd2e9559
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-17 01:06:05 +0200

    Test FileR and FileW

--- test/Wrapping/test.lua
+++ test/Wrapping/test.lua
@@ -14,4 +14,16 @@ function HelloLua(fromC)
     end
 end
 
+local Success = FileW("Text.txt", "New File Contents")
+if Success then
+    LuaPrint("Successfully wrote to 'Text.txt'!");
+    local RSuccess, Contents = FileR("Text.txt");
+    if RSuccess then
+        LuaPrint("Successfully read 'Text.txt'!");
+        print(Contents);
+    end
+else
+    LuaPrint("Failed to open 'Text.txt'!");
+end
+
 HelloLua(false)
