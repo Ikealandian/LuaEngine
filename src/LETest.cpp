3cfbe834673e29e5a490c2d174787e43b79ff682
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-19 00:27:50 +0200

    Implement test print function

--- src/LETest.cpp
+++ src/LETest.cpp
@@ -5,6 +5,42 @@
 #include <LEngine.hpp>
 #include <string>
 
+const char* GetLuaString(LRawState L, int _StackIndex)
+{
+    switch ((LTypes)lua_type(L, _StackIndex))
+    {
+    case LTypes::Number:
+    case LTypes::String:
+        return L_GetString(L, _StackIndex);
+    case LTypes::Boolean:
+        return (L_GetBoolean(L, _StackIndex) ? "true" : "false");
+    case LTypes::Nil:
+        return "nil";
+    default: break;
+    }
+    return "(unprintable data)";
+}
+
+int LuaPrint(LRawState L)
+{
+    // Get # of arguments passed
+    int Arguments = lua_gettop(L);
+
+    // Loop through arguments
+    for (int i = Arguments - 1; i > -1; i--)
+    {
+        // Get string version of the item at the top of the stack
+        const char* String = GetLuaString(L, LUA_TOP - i);
+        // Print the value
+        fputs(String, stdout);
+    }
+
+    // New line
+    fputc('\n', stdout);
+    
+    return 1;
+}
+
 int main(int, char**)
 {
     // Automatically Creates and Destroys
