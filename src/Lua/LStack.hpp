1327d76f39312370d35ec7abba6c3f67f780d141
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-20 02:39:00 +0200

    Implement Push, Pop and Fun Registering and Calling

--- src/Lua/LStack.hpp
+++ src/Lua/LStack.hpp
@@ -14,8 +14,8 @@
 // Include Lua Debug
 #include <Lua/LDebug.hpp>
 
-// Include Push
-// Include Pop
+// typeinfo
+#include <typeinfo>
 
 /* Lua stack positions */
 #define LUA_TOP         -1
@@ -26,7 +26,7 @@
 /**
  * Lua Types
  **/
-enum class LTypes
+enum class LTypes : int
 {
     Number          = LUA_TNUMBER,
     String          = LUA_TSTRING,
@@ -42,7 +42,7 @@ enum class LTypes
 /**
  * Lua Type compare result
  **/
-enum class LCompare
+enum class LCompare : int
 {
     IsBad,
     IsOK,
@@ -50,22 +50,9 @@ enum class LCompare
     IsUserdata
 };
 
-/** Lua Functions { **/
 
-/**
- * Register a C function for Lua
- **/
-void LEF_RegisterCFunction(
-    LRawState _State, const char* _GlobalName, lua_CFunction _CFunction
-);
 
-/**
- * Call Lua function from C
- **/
-//// LEF_CallLuaFunction
-
-/** Lua Functions } **/
-
+////////////////////
 /** Lua Macros { **/
 
 /**
@@ -108,6 +95,174 @@ void LEF_RegisterCFunction(
 #define L_RunScript(L, S)   LuaCall(L, luaL_dostring(L, S))
 #define L_RunFile(L, F)     LuaCall(L, luaL_dofile(L, F))
 
+/**
+ * Lua function macros
+ **/
+#define L_CallFunc
+#define L_RegFunc
+
 /** Lua Macros } **/
+////////////////////
+
+
+////////////////////
+/** Push Value { **/
+
+/**
+ * Push a value onto the Lua stack
+ **/
+template<typename T>
+inline void LuaPushValue(LRawState _State, T _Value)
+{
+    // Unused
+    (void)(_State);
+    (void)(_Value);
+
+    printf("Push\tUnable to push value of type: %s\n", typeid(T).name());
+}
+
+/* LuaPushValue: Int */
+template<>
+inline void LuaPushValue(LRawState _State, int _Number)
+{
+    lua_pushnumber(_State, _Number);
+}
+
+/* LuaPushValue: Double */
+template<>
+inline void LuaPushValue(LRawState _State, double _Number)
+{
+    lua_pushnumber(_State, _Number);
+}
+
+/* LuaPushValue: String */
+template<>
+inline void LuaPushValue(LRawState _State, const char* _String)
+{
+    lua_pushstring(_State, _String);
+}
+
+/* LuaPushValue: Boolean */
+template<>
+inline void LuaPushValue(LRawState _State, bool _Boolean)
+{
+    lua_pushboolean(_State, _Boolean);
+}
+
+/** Push Value } **/
+////////////////////
+
+
+
+//////////////
+/** Push { **/
+
+/**
+ * No more arguments to push to the Lua stack
+ **/
+inline void LuaPush(LRawState) { }
+
+/**
+ * Push multiple values onto the Lua stack
+ **/
+template<typename T, typename ...R>
+inline void LuaPush(LRawState _State, const T& _First, const R&... _Rest)
+{
+    // Push _FirstArg value
+    LuaPushValue(_State, _First);
+
+    // Keep calling LuaPush until all args are pushed
+    LuaPush(_State, _Rest...);
+}
+
+/** Push } **/
+//////////////
+
+
+
+/////////////
+/** Pop { **/
+
+// LuaPopNumber
+inline lua_Number LuaPopNumber(LRawState _State)
+{
+    // Verify top element is a Number
+    if (!LuaVerifyType(_State, (int)LTypes::Number, LUA_TOP))
+        return (lua_Number)NULL;
+    // Get the top element off the Lua stack
+    lua_Number Top = lua_tonumber(_State, LUA_TOP);
+    // Pop off the top element
+    lua_pop(_State, 1);
+    // Return Top number
+    return Top;
+}
+
+// LuaPopString
+inline const char* LuaPopString(LRawState _State)
+{
+    // Verify top element is a String
+    if (!LuaVerifyType(_State, (int)LTypes::String, LUA_TOP))
+        return NULL;
+    // Get the top element off the Lua stack
+    const char* Top = lua_tostring(_State, LUA_TOP);
+    // Pop off the top element
+    lua_pop(_State, 1);
+    // Return Top string
+    return Top;
+}
+
+/** Pop } **/
+/////////////
+
+
+
+///////////////////////
+/** Lua Functions { **/
+
+/**
+ * Lua Function data
+ **/
+typedef struct _LFuncDef
+{
+    const char* Name;
+    int ReturnValues;
+    int InputArguments;
+}LFunction;
+
+/**
+ * Register a C function for Lua
+ **/
+void LEF_RegisterCFunction(
+    LRawState _State, const char* _GlobalName, lua_CFunction _CFunction
+);
+
+/**
+ * LEF_CallLuaFunction Implementation
+ *  Call Lua function from C
+ **/
+template<typename T, typename ...R>
+bool LEF_CallLuaFunction(
+    LRawState _State, const LFunction& _Func, const T& _First, const R& ..._Rest
+) {
+    // Get the function
+    L_GetGlobal(_State, _Func.Name);
+    // Check if its a function
+    if (L_IsFunction(_State, LUA_TOP))
+    {
+        // Push Arguments
+        LuaPush(_State, _First, _Rest...);
+        // Call the function
+        return LuaCall(_State, lua_pcall(_State, _Func.InputArguments, _Func.ReturnValues, 0));
+    } else /* Not a Function */ {
+        // Cant call a nonFunction
+        printf("CallFun\tUnable to call a non-Function value\n");
+        return false;
+    }
+}
+
+/** Lua Functions } **/
+///////////////////////
+
+
 
 #endif
