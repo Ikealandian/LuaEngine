ec28ff2dc870c5409f5828d8b918c5b3175e2425
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-25 00:45:11 +0200

    Add LE_DebugData

--- src/Lua/LDebug.hpp
+++ src/Lua/LDebug.hpp
@@ -11,18 +11,59 @@
 // Include Lua State
 #include <Lua/LState.hpp>
 
+/////////////////////////////
+/** Forward Declaration { **/
+
+/**
+ * Lua types defined in LStack.hpp
+ **/
+enum class LTypes;
+
+/** Forward Declaration } **/
+/////////////////////////////
+
+
+
+////////////////////
+/** Debug Data { **/
+
+/**
+ * Debug data structure containing:
+ *  File path to function
+ *  Function name
+ *  Line number
+ **/
+typedef struct LE_DebugData
+{
+    const char* File;
+    const char* Function;
+    int Line;
+}DebugData;
+
+/**
+ * Automatically generate LE_DebugData values
+ **/
+#define LE_DebugData { __FILE__, __FUNCTION__, __LINE__ }
+
+/** Debug Data { **/
+////////////////////
+
+
+
 /**
  * Verify Lua function results
  **/
 bool LuaCall(
-    LRawState _State, int _LFuncResult
+    LRawState _State, int _LFuncResult,
+    DebugData _DbgData
 );
 
 /**
  * Verify Stack type
  **/
 bool LuaVerifyType(
-    LRawState _State, int _LType, int _Index
+    LRawState _State, LTypes _LType, int _Index,
+    DebugData _DbgData
 );
 
 #endif
