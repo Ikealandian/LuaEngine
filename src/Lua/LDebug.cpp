705d3ed7c7e72e3503244483e54671c551b88ceb
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-25 00:45:20 +0200

    Print the DebugData

--- src/Lua/LDebug.cpp
+++ src/Lua/LDebug.cpp
@@ -15,7 +15,7 @@
  * LuaCall Implementation
  *  Verify Lua function results
  **/
-bool LuaCall(LRawState _State, int _LFuncResult)
+bool LuaCall(LRawState _State, int _LFuncResult, DebugData _DbgData)
 {
     // Lua function was executed successfully
     if (_LFuncResult == LUA_OK)
@@ -24,7 +24,8 @@ bool LuaCall(LRawState _State, int _LFuncResult)
     // Print the Lua error
     // Error is pushed to the top of the Lua stack
     // Get the error string and print it out using stderr
-    fprintf(stderr, "LuaCall\t%s\n", L_GetString(_State, LUA_TOP));
+    fprintf(stderr, "LuaCall\t%s\t[%s:%s:%d]\n", L_GetString(_State, LUA_TOP),
+        _DbgData.File, _DbgData.Function, _DbgData.Line);
 
     // Lua function was no successful
     return false;
@@ -34,15 +35,16 @@ bool LuaCall(LRawState _State, int _LFuncResult)
  * LuaVerifyType Implementation
  *  Verify Stack type
  **/
-bool LuaVerifyType(LRawState _State, int _LType, int _Index)
+bool LuaVerifyType(LRawState _State, LTypes _LType, int _Index, DebugData _DbgData)
 {
     // Get element type
     int Type = lua_type(_State, _Index);
     // Check if element is not equal to comparison type
-    if(Type != _LType)
+    if(Type != (int)_LType)
     {
         // Print T != T
-        fprintf(stderr, "Verify\tType %d != %d\n", Type, _LType);
+        fprintf(stderr, "Verify\tType %d != %d\t[%s:%s:%d]\n", Type, (int)_LType,
+            _DbgData.File, _DbgData.Function, _DbgData.Line);
         return false;
     }
     // Return types match
