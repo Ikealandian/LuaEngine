63e4daf7414c3c7ea0fb7b98ca59a7c58b598df6
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-26 05:08:17 +0200

    Implement LEF_CallLuaFunction

--- src/Lua/LStack.cpp
+++ src/Lua/LStack.cpp
@@ -7,3 +7,21 @@
 
 // Include Lua Debug
 #include <Lua/LDebug.hpp>
+
+/**
+ * LEF_CallLuaFunction Implementation
+ *  Push new global Lua table
+ **/
+IValue LEF_NewGlobalTable(LRawState _State, const char* _Name)
+{
+    // Push new table
+    L_PushTable(_State);
+    // Get table position on Lua stack
+    IValue Position = L_GetTop(_State);
+    // Push value of table index
+    L_PushIValue(_State, Position);
+    // Set the table name
+    L_SetGlobal(_State, _Name);
+    // Return table position
+    return Position;
+}
