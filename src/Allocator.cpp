c5240a1d0e6d2d4e7f059a9e0131c02c68ba543b
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-10-06 00:42:10 +0200

    Implement LE_Allocator

--- src/Allocator.cpp
+++ src/Allocator.cpp
@@ -105,3 +105,125 @@ void Reallocate(Pointer_t* _pPointer, const Bytes_t& _bSize)
 
 /* Memory Functions } */
 ////////////////////////
+
+
+
+/////////////////
+/* Allocator { */
+
+/** Implementation **/
+LE_Allocator::LE_Allocator()
+    : AllocInfo({0,0,0,0})
+{
+}
+
+/** Implementation **/
+LE_Allocator::~LE_Allocator()
+{
+    /* Free all Allocations */
+    FreeAll();
+}
+
+/** Implementation                                  **/
+/** Frees all allocated memory by this Allocator    **/
+void LE_Allocator::FreeAll(void)
+{
+    // Loop through all active allocations
+    for (auto& Alloc : Allocs)
+    {
+        // Free current allocation
+        Free(reinterpret_cast<Pointer_t>(Alloc.first));
+    }
+
+    // Clear all
+    Allocs.clear();
+}
+
+/** Implementation              **/
+/** Print out Allocation data   **/
+void LE_Allocator::PrintInfo(void)
+{
+    printf("Alloc:\n\tLifetime: %dB\n\tTotal: %dB\n\tActive: %ld\n",
+     AllocInfo.LifetimeAllocated,
+     AllocInfo.TotalAllocated,
+     Allocs.size()
+    );
+}
+
+/** Implementation  **/
+/** Calls Allocate  **/
+Pointer_t LE_Allocator::Alloc(const Bytes_t& _bSize)
+{
+    // Allocate
+    Pointer_t Allocation = Allocate(_bSize);
+    
+    // Save address and size in active allocations
+    Allocs[reinterpret_cast<Address_t>(Allocation)] = _bSize;
+
+    // Add to total allocated
+    AllocInfo.TotalAllocated += _bSize;
+    AllocInfo.LifetimeAllocated += _bSize;
+
+    // Return pointer
+    return Allocation;
+}
+
+/** Implementation      **/
+/** Calls Reallocate    **/
+Pointer_t LE_Allocator::Realloc(Pointer_t _pPointer, const Bytes_t& _bSize)
+{
+    // Get Address_t
+    Address_t Address = reinterpret_cast<Address_t>(_pPointer);
+
+    // Get the old size of pointer
+    Bytes_t OldSize = Allocs[Address];
+
+    // Get size difference
+    Bytes_t DifSize = _bSize - OldSize;
+
+    // Add the size difference
+    AllocInfo.LifetimeAllocated += (DifSize > 0) ? DifSize : 0;
+    AllocInfo.TotalAllocated += DifSize;
+
+    // Reallocate
+    Pointer_t NewPointer = Reallocate(_pPointer, _bSize);
+
+    // If the new pointer is different
+    if (NewPointer != _pPointer)
+    {
+        // Get the new pointers address
+        Address_t NewAddress = reinterpret_cast<Address_t>(NewPointer);
+
+        // Erase old data
+        Allocs.erase(Address);
+
+        // Set new data
+        Allocs[NewAddress] = _bSize;
+    }
+
+    // Return new pointer
+    return NewPointer;
+}
+
+/** Implementation      **/
+/** Calls Deallocate    **/
+void LE_Allocator::Free(Pointer_t _pPointer)
+{
+    // Get Address_t
+    Address_t Address = reinterpret_cast<Address_t>(_pPointer);
+
+    // Get size of pointer
+    Bytes_t Size = Allocs[Address];
+
+    // Sub from total allocated
+    AllocInfo.TotalAllocated -= Size;
+
+    // Deallocate
+    Deallocate(_pPointer);
+
+    // Remove from active allocations
+    Allocs.erase(Address);
+}
+
+/* Allocator } */
+/////////////////
